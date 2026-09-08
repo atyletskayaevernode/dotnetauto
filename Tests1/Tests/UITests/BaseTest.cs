9679614471d2e726406d49d26c9c5bcaa73f6857
a.tyletskaya <a.tyletskaya@evernode.tech>
2026-09-08 20:56:37 +0200

    lesson 20

---
+++ Tests1/Tests/UITests/BaseTest.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Playwright;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Tests1.ForUI.Framework;
+
+namespace Tests1.Tests.UITests
+{
+    public class BaseTest
+    {
+        protected IPage Page { get; private set; }
+        protected PlaywrightFixture Fixture { get; }
+
+        protected BaseTest()
+        {
+            Fixture = new PlaywrightFixture();
+            Fixture.InitializeAsync().Wait();
+        }
+
+        [SetUp]
+        public async Task SetUp()
+        {
+            Page = await Fixture.Browser.NewPageAsync(new BrowserNewPageOptions
+            {
+                ViewportSize = null
+            });
+        }
+
+        [TearDown]
+        public async Task TearDown()
+        {
+            await Page.CloseAsync();
+        }
+
+        [OneTimeTearDown]
+        public async Task GlobalTearDown()
+        {
+            await Fixture.DisposeAsync();
+        }
+    }
+}
---
+++ Tests1/Tests/UITests/HerokuTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tests1.Tests.UITests
+{
+    public class HerokuTests : BaseTest
+    {
+        [Test]
+        public async Task CheckBoxTest()
+        {
+            await Page.GotoAsync("https://the-internet.herokuapp.com/checkboxes");
+            var first = Page.Locator("input[type='checkbox']").Nth(0);
+            await first.CheckAsync();
+            (await first.IsCheckedAsync()).Should().BeTrue();
+        }
+    }
+}
